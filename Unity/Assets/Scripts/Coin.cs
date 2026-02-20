ec8b56942d766a5fa3d1add5819b177c787f9bfb
typical-u <calebchenz27@gmail.com>
2026-02-20 10:48:37 -0700

    Added Unity Coin + Player Controller

---
+++ Unity/Assets/Scripts/Coin.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("You Touched The Coin!");
+        }
+    }
+}
