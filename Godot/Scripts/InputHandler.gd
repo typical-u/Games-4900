07bc0d34bb2bafeac7de34a543984eb9abbbc3e2
typical-u <calebchenz27@gmail.com>
2026-02-27 15:31:29 -0700

    DownGraded Unity, InputMap Godot
    Created a simple script to read inputs
    In Project Settings, set keys for InputMap

---
+++ Godot/Scripts/InputHandler.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var attack_held := false
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("attack"):
+		attack_held = true
+		print("Attack Pressed")
+	
+	if Input.is_action_just_released("attack"):
+		attack_held = false
+		print("Attack Released")
+		
+	if attack_held:
+		print("Attack Held")
+
+	var move := Input.get_vector("move_down","move_up","move_left","move_right")
+	if move != Vector2.ZERO:
+		print("Move", move)
