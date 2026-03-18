c557c88536c3df44ce73e3bd5bf727d89a7138d6
typical-u <calebchenz27@gmail.com>
2026-03-18 15:43:09 -0600

    Godot Attack!!

--- Godot/Scripts/InputHandler.gd
+++ Godot/Scripts/InputHandler.gd
@@ -4,8 +4,8 @@ var attack_held := false
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("attack"):
-		attack_held = true
-		print("Attack Pressed")
+		#attack_held = true
+		print("Attack!!")
 	
 	if Input.is_action_just_released("attack"):
 		attack_held = false
